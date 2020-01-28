b6cc86e0aff0378aab760171f975818b05471e6c
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-01-28 12:25:27 +0100

    Debut Forum

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<link rel="stylesheet" type="text/css" href="forum.css">
+</head>
+<body>
+	<header>
+		
+	</header>
+
+
+	<main>
+		
+	</main>
+
+
+	<footer>
+		
+	</footer>
+
+</body>
+</html>
