b7df124f6b9b89046b5032ca1822301ec3e4ead8
Paul Le <paulloukyle@gmail.com>
2020-01-29 11:18:41 +0100

    Début du projet en HTML CSS

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Forum index</title>
+        <link rel="stylesheet" href="index.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <header>
+            <section>
+                <img src="Images/logoforumheader.png">
+            </section>
+            <section>
+                <article id="coheaderflex">
+                    <p id="cocenter">- Connexion -</p>
+                    <form>
+                        <input id="connexioninput" type="text" placeholder="Identifiant" name="login" required><br><br>
+                        <input id="connexioninput" type="password" placeholder="Mot de passe" name="password" required><br><br>
+                        <input id="connexioninput" type="submit" name="connexion" value="Se connecter">       
+                    </form>
+                </article>
+            </section>
+        </header>
+        <nav>
+            <ul id="navul">
+                <li>Accueil</li>
+                <li>Inscription</li>
+                <li>Profil</li>
+            </ul>
+        </nav>
+        <main>
+            <section id="reglesmainsection">
+                <section>
+                    <h1 id="regleh1">&nbsp;&nbsp;Règles du forum</h1>
+                </section>
+                <section id="endessousdutitreflex">
+                    <section id="topicicon">
+                        <img src="Images/nonewmessage.png">
+                    </section> 
+                    <section id="topicflex1">
+                        <section id="topicflex2">
+                            <article>A lire avant de poster !</article>
+                        </section>
+                        <section id="topicflex3">
+                            <article>Code de conduite</article>
+                        </section>
+                    </section>
+                    <section>
+                        <article>
+                            1 message<br>1 sujet
+                        </article>
+                    </section>
+                <section>
+            </section>
+        </main>
